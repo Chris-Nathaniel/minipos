e039dac4864a49eb3098f5d7e68f001085943424
Chris <Chris.nathaniel29@gmail.com>
2025-02-17 16:15:37 +0700

    mai

--- Minipos.bat
+++ Minipos.bat
@@ -11,6 +11,6 @@ start /min "" "%SCRIPT_DIR%connecttongrok.bat"
 timeout /t 4 /nobreak >nul
 
 :: Start the main app
-start chrome.exe https://amused-fit-wasp.ngrok-free.app/
+start "" "%SCRIPT_DIR%miniposapp.lnk"
 
 exit
